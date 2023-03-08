e9eae4fef0ac3f12fb450c9d1e5c1e926dbf1a8a
Scott <sm_peterson@yahoo.com>
2023-03-08 17:53:25 -0600

    changes after testing

---
+++ lambdas/civo/civo.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+def lambda_handler(event, context):   
+    # Define the base URL and starting page number
+    base_url = 'https://www.civo.com/blog'
+    page_num = 1
+
+    # Create an empty list to store the full URLs of all the links
+    full_links = []
+
+    while True:
+        # Construct the URL for the current page
+        url = f'{base_url}?page={page_num}'
+
+        # Send a GET request to the URL
+        response = requests.get(url)
+
+        # Parse the HTML content with BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find the div containing the links
+        div = soup.select_one('body > main > div.container > div > div:nth-child(1) > div > div')
+
+        # Extract all the links within the div and add them to the full_links list
+        links = div.find_all('a')
+        full_links += [f'https://www.civo.com{link.get("href")}' for link in links]
+
+        # Check if there are no blog posts on this page (i.e., we've reached the last page)
+        if len(links) == 0:
+            break
+
+        # Increment the page number and continue to the next page
+        page_num += 1
+
+    # Print the full URLs of all the links
+    for link in full_links:
+        print(link)
+        return(full_links)
---
+++ pulumi/__main__.py
@@ -0,0 +1,35 @@
+import pulumi
+import pulumi_aws as aws
+
+# Create a new IAM role for the Lambda function
+role = aws.iam.Role('lambda-role',
+    assume_role_policy="""{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                }
+            }
+        ]
+    }""",
+)
+
+# Attach a basic execution policy to the role
+aws.iam.RolePolicyAttachment('lambda-policy',
+    policy_arn='arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
+    role=role,
+)
+
+# Create a new Lambda function
+function = aws.lambda_.Function('my-lambda-function',
+    code=pulumi.AssetArchive({
+        '.': pulumi.FileArchive('../lambdas/civo/my-deployment-package.zip'),
+    }),
+    handler='civo.lambda_handler',
+    role=role.arn,
+    runtime='python3.8',
+    timeout=30,
+)
